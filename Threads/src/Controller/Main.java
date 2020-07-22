bfb82f130296b84405e24170f2a26dcfbd58785a
Leonardo Horacio <leonardohoracio81@gmail.com>
2020-07-21 22:42:45 -0300

    classe principal do projeto

---
+++ Threads/src/Controller/Main.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import View.MinhaThread;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		MinhaThread t1 = new MinhaThread("Thread 1", 1000);
+
+		MinhaThread t2 = new MinhaThread("Thread 2", 2000);
+
+		MinhaThread t3 = new MinhaThread("Thread 3", 1000);
+		
+		
+
+	}
+
+}
